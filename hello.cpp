983f59145348bbf567cbe8d1c88caf297c4cef6d
luisbarrera1 <labarrera@mail.csuchico.edu>
2019-09-04 16:35:42 -0700

    changed the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
+// Author: Luis Barrera
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Luis. What is yours?" << std::endl;
     std::cin >> input;
-    std::cout << "Hello, " << input << std::endl;
+    std::cout << "Hello," << input << std::endl;
     return 0;
 }
